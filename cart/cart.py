88203a54122326d2f9cf6d925e078fa96fe91c9d
Рустам Каюмов <rustrnb@yandex.ru>
2020-10-06 00:52:31 +0500

    Добавление шаблонов корзины

--- cart/cart.py
+++ cart/cart.py
@@ -18,18 +18,18 @@ class Cart(object):
         """Добавление товара в корзину или обновление его количества"""
         product_id = str(product.id)
         if product_id not in self.cart:
-            self.cart[prodict_id] = {'quantity': 0, 'price': str(product.price)}
+            self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
         if update_quantity:
             self.cart[product_id]['quantity'] = quantity
         else:
-            self.cart[product_id]['quantity'] +=quantity
+            self.cart[product_id]['quantity'] += quantity
         self.save()
 
     def save(self):
         # помечается сессия как измененная
         self.session.modified = True
 
-    def remove(self):
+    def remove(self, product):
         """Удаление товара из корзины"""
         product_id = str(product.id)
         if product_id in self.cart:
@@ -48,7 +48,7 @@ class Cart(object):
         for item in cart.values():
             item['price'] = Decimal(item['price'])
             item['total_price'] = item['price'] * item['quantity']
-        yield item
+            yield item
 
     def __len__(self):
         """Возвращение общего кол-ва товаров в корзине"""
