27fd586566b7e0c97d435e03ef6648da3904ccea
Рустам Каюмов <rustrnb@yandex.ru>
2020-10-07 01:07:48 +0500

    fix template

--- orders/views.py
+++ orders/views.py
@@ -21,7 +21,7 @@ def order_create(request):
 
             order_created.delay(order.id)
 
-            return render(request, 'orders/order/create.html', {'order': order})
+            return render(request, 'orders/order/created.html', {'order': order})
     else:
         form = OrderCreateForm()
     return render(request, 'orders/order/create.html', {'cart':cart, 'form': form})
