4039904a7078463d2768278bcb303ff30e2907d5
Рустам Каюмов <rustrnb@yandex.ru>
2020-10-14 23:42:44 +0500

    Экспорт в CSV

--- orders/admin.py
+++ orders/admin.py
@@ -14,3 +14,27 @@ class OrderAdmin(admin.ModelAdmin):
                     'city', 'paid', 'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
+
+    def export_to_csv(modeladmin, request, queryset) :
+        opts = modeladmin.model._meta
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment;' \
+                                          'filename={}.csv'.format(opts.verbose_name)
+        writer = csv.writer(response)
+        fields = [field for field in opts.get_fields() if not field.many_to_many \
+                  and not field.one_to_many]
+        # Записываем первую строку с заголовками полей.
+        writer.writerow([field.verbose_name for field in fields])
+        # Записываем данные.
+        for obj in queryset :
+            data_row = []
+            for field in fields :
+                value = getattr(obj, field.name)
+                if isinstance(value, datetime.datetime) :
+                    value = value.strftime('%d/%m/%Y')
+                data_row.append(value)
+            writer.writerow(data_row)
+        return response
+
+    export_to_csv.short_description = 'Экспортировать в CSV'
